dbc4b51bd16e3639fc7f7a3da0333bc73a41970b
saadm <saadmeddiche2004201@gmail.com>
2024-02-15 09:52:20 +0100

    Improve the FileStorageService.storeFile()

--- mt-core/src/main/java/com/MeddicheTruck/mtcore/services/FileStorageService.java
+++ mt-core/src/main/java/com/MeddicheTruck/mtcore/services/FileStorageService.java
@@ -2,25 +2,23 @@ package com.MeddicheTruck.mtcore.services;
 
 import com.google.common.io.Files;
 import lombok.SneakyThrows;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
+
 
 public class FileStorageService {
 
-    private static final String DEFAULT_FILE_STORAGE_PATH = "/images/pieces/";
 
+    private static final String DEFAULT_RESOURCE_PATH = "mt-main/src/main/resources/";
+    private static final String DEFAULT_FILE_STORAGE_PATH = "/images/";
     private static final String EXTENSION = ".png";
 
     @SneakyThrows
-    public static String storeFile(byte[] photoInBase64Format, String photoName)  {
+    public static String storeFile(byte[] photoInBase64Format, String photoName , String folderName)  {
 
-        String fullPath = ResourceUtils.getURL("mt-main/src/main/resources/") + "images/pieces/" + photoName + EXTENSION;
-
-        fullPath = fullPath.replace("file:/", "");
+        String fullPath = getFullPath("pieces" , photoName);
 
         File image = new File(fullPath);
 
@@ -32,5 +30,32 @@ public class FileStorageService {
     }
 
     public static void deleteFile(String photoPath) {
+    }
+
+    private static String getFullPath(String folderName , String photoName) {
+
+        StringBuilder fullPath = new StringBuilder();
+
+        try {
+            fullPath.append(ResourceUtils.getURL(DEFAULT_RESOURCE_PATH));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("[Warning] The resource path was not found , that could cause a problem with the file storage service");
+        }
+
+        fullPath.append(DEFAULT_FILE_STORAGE_PATH);
+
+        fullPath.append("/");
+
+        fullPath.append(folderName);
+
+        fullPath.append("/");
+
+        fullPath.append(photoName);
+
+        fullPath.append(EXTENSION);
+
+        // Remove the file:/ from the path because ResourceUtils.getURL() returns a path with file:/ at the beginning
+        return fullPath.toString().replace("file:/", "");
+
     }
 }
