a198fbc4b5c2fb925bc63f790d11329786e77c3d
saadm <saadmeddiche2004201@gmail.com>
2024-02-15 16:50:07 +0100

    Check If The images are not null before looping on them

--- mt-main/src/main/java/com/MeddicheTruck/mtmain/listeners/PieceListener.java
+++ mt-main/src/main/java/com/MeddicheTruck/mtmain/listeners/PieceListener.java
@@ -4,13 +4,28 @@ import com.MeddicheTruck.mtcore.services.FileStorageService;
 import com.MeddicheTruck.mtmain.entities.Piece;
 import jakarta.persistence.PrePersist;
 
-public class PieceListener {
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
+@Component @NoArgsConstructor
+public class PieceListener {
+    private FileStorageService fss;
+    PieceListener(FileStorageService fss) {
+        this.fss = fss;
+    }
     @PrePersist
     public void prePersist(Piece piece) {
-        piece.getImages().forEach(image -> {
-            image.setPiece(piece);
-            image.setPhotoPath(FileStorageService.storeFile(image.getPhotoInBase64Format(), image.getName() , "pieces"));
-        });
+
+        Optional.ofNullable(piece.getImages())
+            .ifPresent(images -> images.forEach(
+                    image -> {
+                        image.setPiece(piece);
+                        image.setPhotoPath(fss.storeFile(image.getPhotoInBase64Format(), image.getName() , "pieces"));
+                    }
+            ));
+
     }
 }
