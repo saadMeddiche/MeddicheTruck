6ae4d51aa5a4ce50037cd2cc36a45e33f0a9b7f8
saadm <saadmeddiche2004201@gmail.com>
2024-02-15 10:51:39 +0100

    Remove the @SneakyThrows annotation , and handle the exceptions

--- mt-core/src/main/java/com/MeddicheTruck/mtcore/services/FileStorageService.java
+++ mt-core/src/main/java/com/MeddicheTruck/mtcore/services/FileStorageService.java
@@ -1,11 +1,11 @@
 package com.MeddicheTruck.mtcore.services;
 
 import com.google.common.io.Files;
-import lombok.SneakyThrows;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
 
@@ -17,25 +17,37 @@ public class FileStorageService {
     private static final String DEFAULT_FILE_STORAGE_PATH = "/images/";
     private static final String EXTENSION = ".png";
 
-    @SneakyThrows
-    public static String storeFile(byte[] photoInBase64Format, String photoName , String folderName)  {
+    public static String storeFile(byte[] fileInBase64Format, String fileName , String folderName)  {
 
-        String fullPath = getFullPath("pieces" , photoName);
+        String fullPath = getFullPath("pieces" , fileName);
 
-        File image = new File(fullPath);
+        File file = new File(fullPath);
 
-        Files.createParentDirs(image);
+        createParentDirectories(file);
 
-        Files.write(photoInBase64Format, image);
+        writeToFile(fileInBase64Format, file);
 
         return fullPath;
     }
 
-    public static void deleteFile(String photoPath) {
+    private static void createParentDirectories(File file) {
+        try {
+            Files.createParentDirs(file);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create parent directories for file: " + file.getAbsolutePath(), e);
+        }
     }
 
-    private static String getFullPath(String folderName , String photoName) {
+    private static void writeToFile(byte[] fileInBase64Format, File file) {
+        try {
+            Files.write(fileInBase64Format, file);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write file: " + file.getAbsolutePath(), e);
+        }
+    }
 
+
+    private static String getFullPath(String folderName , String photoName) {
 
         try {
            URL resourceUrl = ResourceUtils.getURL(DEFAULT_RESOURCE_PATH);
@@ -43,6 +55,7 @@ public class FileStorageService {
            String resourcePath = Paths.get(resourceUrl.toURI()).toString();
 
             return Paths.get(resourcePath, DEFAULT_FILE_STORAGE_PATH, folderName, photoName + EXTENSION).toString();
+
         } catch (FileNotFoundException e) {
             throw new RuntimeException("[Warning] The resource path was not found , that could cause a problem with the file storage service" , e);
         } catch (Exception e) {
