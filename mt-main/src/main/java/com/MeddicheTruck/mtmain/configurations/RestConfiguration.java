e419175c7e0f97ee945ce77442c0273aba0604b0
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-03-27 13:34:49 +0000

    Expose id of Vehicle and its images

--- mt-main/src/main/java/com/MeddicheTruck/mtmain/configurations/RestConfiguration.java
+++ mt-main/src/main/java/com/MeddicheTruck/mtmain/configurations/RestConfiguration.java
@@ -2,6 +2,8 @@ package com.MeddicheTruck.mtmain.configurations;
 
 import com.MeddicheTruck.mtmain.entities.Piece;
 import com.MeddicheTruck.mtmain.entities.PieceImage;
+import com.MeddicheTruck.mtmain.entities.Vehicle;
+import com.MeddicheTruck.mtmain.entities.VehicleImage;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -15,5 +17,7 @@ public class RestConfiguration implements RepositoryRestConfigurer {
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config , CorsRegistry cors) {
         config.exposeIdsFor(Piece.class);
         config.exposeIdsFor(PieceImage.class);
+        config.exposeIdsFor(Vehicle.class);
+        config.exposeIdsFor(VehicleImage.class);
     }
 }
