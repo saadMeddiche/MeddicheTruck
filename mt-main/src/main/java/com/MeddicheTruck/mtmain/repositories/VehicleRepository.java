5dbf9fcd0fbcffd687a4eae72f7554af2a60c94a
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-03-27 13:15:28 +0000

    Change matricule to plate

--- mt-main/src/main/java/com/MeddicheTruck/mtmain/repositories/VehicleRepository.java
+++ mt-main/src/main/java/com/MeddicheTruck/mtmain/repositories/VehicleRepository.java
@@ -11,7 +11,7 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
     @Query("SELECT v FROM Vehicle v WHERE" +
             " v.model LIKE %:searchTerm% OR" +
-            " v.matricule LIKE %:searchTerm% OR" +
+            " v.plate LIKE %:searchTerm% OR" +
             " CAST(v.type AS string) LIKE %:searchTerm% OR" +
             " CAST(v.engineType AS string) LIKE %:searchTerm%")
     Page<Vehicle> dynamicSearch(@Param("searchTerm") String searchTerm, Pageable pageable);
