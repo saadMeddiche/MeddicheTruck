c0341950e52319e02e866d9a9ddf80225b4b6894
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-04-09 17:10:09 +0000

    update findPhoneNumbersByPersonId

--- mt-main/src/main/java/com/MeddicheTruck/mtmain/repositories/PhoneNumberRepository.java
+++ mt-main/src/main/java/com/MeddicheTruck/mtmain/repositories/PhoneNumberRepository.java
@@ -16,13 +16,22 @@ public interface PhoneNumberRepository extends BaseRepository<PhoneNumber>{
             " CAST(p.type AS string ) LIKE %:searchTerm% OR" +
             " p.person.firstName LIKE %:searchTerm% OR" +
             " p.person.lastName LIKE %:searchTerm% OR" +
-            " p.person.middleName  LIKE %:searchTerm%")
+            " p.person.middleName  LIKE %:searchTerm%"
+    )
     Page<PhoneNumber> dynamicSearch(@Param("searchTerm") String searchTerm, Pageable pageable);
 
-    @Query("SELECT p FROM PhoneNumber p WHERE p.person.id = :personId AND p.number LIKE %:searchTerm%")
+    @Query("SELECT p FROM PhoneNumber p WHERE" +
+            " p.person.id = :personId AND (" +
+            " p.number LIKE %:searchTerm% OR" +
+            " CAST(p.type AS string ) LIKE %:searchTerm% OR" +
+            " p.person.firstName LIKE %:searchTerm% OR" +
+            " p.person.lastName LIKE %:searchTerm% OR" +
+            " p.person.middleName  LIKE %:searchTerm% )"
+    )
     Page<PhoneNumber> findPhoneNumbersByPersonId(Long personId ,@Param("searchTerm") String searchTerm, Pageable pageable);
 
     Boolean existsPhoneNumberByNumber(String number);
 
     Boolean existsPhoneNumberByNumberAndIdNot(String number, Long id);
+
 }
