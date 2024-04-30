2c4ca35427bfdd05be6e971f7d20b2b40b98ac2b
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-04-30 13:52:26 +0100

    add queries for transactions

--- mt-main/src/main/java/com/MeddicheTruck/mtmain/repositories/PieceTransactionRepository.java
+++ mt-main/src/main/java/com/MeddicheTruck/mtmain/repositories/PieceTransactionRepository.java
@@ -2,7 +2,6 @@ package com.MeddicheTruck.mtmain.repositories;
 
 import com.MeddicheTruck.mtcore.base.BaseRepository;
 import com.MeddicheTruck.mtmain.entities.PieceTransaction;
-import com.MeddicheTruck.mtmain.entities.VehicleTransaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -22,4 +21,28 @@ public interface PieceTransactionRepository extends BaseRepository<PieceTransact
             "pt.person.fullName LIKE %:searchTerm% OR "+
             "CAST(pt.price AS string) LIKE %:searchTerm% ")
     Page<PieceTransaction> dynamicSearch(@Param("searchTerm") String searchTerm, Pageable pageable);
+
+    @Query("SELECT pt FROM PieceTransaction pt WHERE " +
+            "pt.piece.id = :pieceId AND (" +
+            "CAST(pt.id AS string ) = :searchTerm OR " +
+            "CAST(pt.date AS string) LIKE %:searchTerm% OR "+
+            "CAST(pt.time AS string) LIKE %:searchTerm% OR "+
+            "CAST(pt.type AS string) LIKE %:searchTerm% OR "+
+            "pt.description LIKE %:searchTerm% OR "+
+            "pt.piece.name LIKE %:searchTerm% OR "+
+            "pt.person.fullName LIKE %:searchTerm% OR "+
+            "CAST(pt.price AS string) LIKE %:searchTerm% )")
+    Page<PieceTransaction> getPieceTransactionByPiece(Long pieceId, @Param("searchTerm") String searchTerm, Pageable pageable);
+
+    @Query("SELECT pt FROM PieceTransaction pt WHERE " +
+            "pt.person.id = :personId AND (" +
+            "CAST(pt.id AS string ) = :searchTerm OR " +
+            "CAST(pt.date AS string) LIKE %:searchTerm% OR "+
+            "CAST(pt.time AS string) LIKE %:searchTerm% OR "+
+            "CAST(pt.type AS string) LIKE %:searchTerm% OR "+
+            "pt.description LIKE %:searchTerm% OR "+
+            "pt.piece.name LIKE %:searchTerm% OR "+
+            "pt.person.fullName LIKE %:searchTerm% OR "+
+            "CAST(pt.price AS string) LIKE %:searchTerm% )")
+    Page<PieceTransaction> getPieceTransactionByPerson(Long personId, @Param("searchTerm") String searchTerm, Pageable pageable);
 }
--- mt-main/src/main/java/com/MeddicheTruck/mtmain/repositories/VehicleTransactionRepository.java
+++ mt-main/src/main/java/com/MeddicheTruck/mtmain/repositories/VehicleTransactionRepository.java
@@ -24,4 +24,34 @@ public interface VehicleTransactionRepository extends BaseRepository<VehicleTran
             "vt.person.fullName LIKE %:searchTerm% OR "+
             "CAST(vt.price AS string) LIKE %:searchTerm% ")
     Page<VehicleTransaction> dynamicSearch(@Param("searchTerm") String searchTerm, Pageable pageable);
+
+    @Query("SELECT vt FROM VehicleTransaction vt WHERE " +
+            "vt.vehicle.id = :vehicleId AND (" +
+            "CAST(vt.id AS string ) = :searchTerm OR " +
+            "CAST(vt.date AS string) LIKE %:searchTerm% OR "+
+            "CAST(vt.time AS string) LIKE %:searchTerm% OR "+
+            "CAST(vt.type AS string) LIKE %:searchTerm% OR "+
+            "vt.description LIKE %:searchTerm% OR "+
+            "vt.vehicle.model LIKE %:searchTerm% OR "+
+            "vt.vehicle.plate LIKE %:searchTerm% OR "+
+            "CAST(vt.vehicle.type AS string) LIKE %:searchTerm% OR "+
+            "CAST(vt.vehicle.engineType AS string ) LIKE %:searchTerm% OR "+
+            "vt.person.fullName LIKE %:searchTerm% OR "+
+            "CAST(vt.price AS string) LIKE %:searchTerm% )")
+    Page<VehicleTransaction> getVehicleTransactionByVehicle(Long vehicleId, @Param("searchTerm") String searchTerm, Pageable pageable);
+
+    @Query("SELECT vt FROM VehicleTransaction vt WHERE " +
+            "vt.person.id = :personId AND (" +
+            "CAST(vt.id AS string ) = :searchTerm OR " +
+            "CAST(vt.date AS string) LIKE %:searchTerm% OR "+
+            "CAST(vt.time AS string) LIKE %:searchTerm% OR "+
+            "CAST(vt.type AS string) LIKE %:searchTerm% OR "+
+            "vt.description LIKE %:searchTerm% OR "+
+            "vt.vehicle.model LIKE %:searchTerm% OR "+
+            "vt.vehicle.plate LIKE %:searchTerm% OR "+
+            "CAST(vt.vehicle.type AS string) LIKE %:searchTerm% OR "+
+            "CAST(vt.vehicle.engineType AS string ) LIKE %:searchTerm% OR "+
+            "vt.person.fullName LIKE %:searchTerm% OR "+
+            "CAST(vt.price AS string) LIKE %:searchTerm% )")
+    Page<VehicleTransaction> getVehicleTransactionByPerson(Long personId, @Param("searchTerm") String searchTerm, Pageable pageable);
 }
